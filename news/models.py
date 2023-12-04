d92526b300a788ee6b04742cbefbf1192baa0ec7
Beatriz Vago <beatriz@Rafaels-MBP.Home>
2023-12-04 16:05:34 +0000

    Alterações nas models user e news

--- news/models.py
+++ news/models.py
@@ -10,20 +10,20 @@ class Category(models.Model):
 
 class User(models.Model):
     name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
     password = models.CharField(max_length=200)
     role = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
-    
+
 
 class News(models.Model):
     title = models.CharField(max_length=200, validators=[title_validator])
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateField()
-    image = models.ImageField(upload_to="img/")
+    image = models.ImageField(upload_to="img/", blank=True)
     categories = models.ManyToManyField("Category")
 
     def __str__(self):
