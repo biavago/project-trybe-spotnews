448d77ddb85b2f901ea37454f723dfed93fdec01
Beatriz Vago <beatriz@Rafaels-MBP.Home>
2023-12-04 15:53:18 +0000

    req 1, 2 e 3 - criação das models e migrations

---
+++ news/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from news.validation import title_validator
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+
+class News(models.Model):
+    title = models.CharField(max_length=200, validators=[title_validator])
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField()
+    image = models.ImageField(upload_to="img/")
+    categories = models.ManyToManyField("Category")
+
+    def __str__(self):
+        return self.title
