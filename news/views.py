7fa01dd459e7395bef144277b182fdef011f55a1
Beatriz Vago <beatriz@Rafaels-MBP.Home>
2023-12-05 22:31:41 +0000

    refatorando os requisitos 4, 5 e 6

--- news/views.py
+++ news/views.py
@@ -4,22 +4,27 @@ from django.shortcuts import redirect, render, get_object_or_404
 
 
 def home(request):
-    context = {"news": News.objects.all()}
+    context = {
+        "news": News.objects.all()
+    }
     return render(request, "home.html", context)
 
 
 def news_details(request, id):
     obj_news = get_object_or_404(News, id=id)
-    all_categories = obj_news.categories.all()
-    context = {"new": obj_news, "categories": all_categories}
+    context = {
+        "new": obj_news, 
+        "categories": obj_news.categories.all()
+    }
     return render(request, "news_details.html", context)
 
 
 def categories_form(request):
-    context = {"form": CategoriesForms()}
-
     if request.method == "POST":
-        name_tag = request.POST.get("name")
-        Category.objects.create(name=name_tag)
+        Category.objects.create(name=request.POST.get("name"))
         return redirect("home-page")
+
+    context = {
+        "form": CategoriesForms()
+    }
     return render(request, "categories_form.html", context)
