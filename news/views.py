0340d51c1b9bd433c0166b0533d560e39632f4d9
Beatriz Vago <beatriz@Rafaels-MBP.Home>
2023-12-05 19:22:27 +0000

    req 04

--- news/views.py
+++ news/views.py
@@ -1,3 +1,7 @@
+from news.models import News
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    context = {"news": News.objects.all()}
+    return render(request, "home.html", context)
